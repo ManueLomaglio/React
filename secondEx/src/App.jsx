163435ac9bd5bd7600f97d06d0fd679b37a29e61
Manuel Lomaglio <manuelomaglio@gmail.com>
2025-02-10 07:50:40 +0100

    2ex

---
+++ secondEx/src/App.jsx
@@ -0,0 +1,8 @@
+import Hello from "./Hello"
+
+
+function App() {
+ return <div> <Hello/> </div>
+} 
+
+export default App
---
+++ secondEx/src/Hello.jsx
@@ -0,0 +1,7 @@
+import Message from "./message";
+
+function Hello () {
+    return <h2>Hello,World! <Message/>  </h2>
+    
+}
+export default Hello;
