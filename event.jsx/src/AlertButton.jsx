bdd7f79ee0d5f05b867a3843e928f671678cc903
Manuel Lomaglio <manuelomaglio@gmail.com>
2025-02-12 06:47:23 +0100

    ex5

---
+++ event.jsx/src/AlertButton.jsx
@@ -0,0 +1,5 @@
+function AlertButton({ eventHandler }) {
+  return <button onClick={eventHandler}>Vuoi sapere che ore sono?</button>;
+}
+export default AlertButton;
+//che succede se implemento la funzione in un file a parte ?
---
+++ event.jsx/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AlertButton from "./AlertButton";
+
+export default function App() {
+ 
+  function HandleOnClick(){
+    const now= new Date()
+    alert(`the time tight now is: ${now.toLocaleTimeString()}`)
+}
+
+  return (
+    <div>
+      <h1>HELLO!
+      </h1>
+      <AlertButton eventHandler={HandleOnClick}/>
+    </div>
+  );
+}
