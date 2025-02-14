a56f4fc442bd78dcf86a6765c71043d88a2ef0aa
Manuel Lomaglio <manuelomaglio@gmail.com>
2025-02-14 10:56:01 +0100

    list ex

---
+++ list/src/App.jsx
@@ -0,0 +1,14 @@
+import Color from "./Color"
+
+
+
+
+function App() {
+ const color=[{id:1 , name:"carl"},{id:2 , name:"esteban"},{id:3 , name:"xin yan"}]
+  return(
+    <Color color={color}/>
+  )
+  
+}
+
+export default App
---
+++ list/src/Color.jsx
@@ -0,0 +1,9 @@
+export default function Color({ color }) {
+  return (
+    <ul>
+      {color.map((color) => (
+        <li key={color.id}> {color.name} </li>
+      ))}
+    </ul>
+  );
+}
