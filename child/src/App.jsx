d8f74e37d86ca48be6b6bfdca42692554e6e34bf
Manuel Lomaglio <manuelomaglio@gmail.com>
2025-02-19 11:57:41 +0100

    child

---
+++ child/src/App.jsx
@@ -0,0 +1,15 @@
+import Container from "./Container"
+
+
+
+function App() {
+  
+
+  return (<Container title={"Title container"}>
+    <h1>Hello World!</h1>
+  </Container>
+    
+  )
+}
+
+export default App
---
+++ child/src/Container.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function Container({ title, children }) {
+  const [collapsed, setCollapsed] = useState(false);
+  const handleColapsed = () => {
+    setCollapsed(!collapsed);
+  };
+  return (
+    <div className="container">
+      {" "}
+      <h2 onClick={handleColapsed}>{title}</h2>
+      {!collapsed && <div>{children}</div>}
+    </div>
+  );
+}
+export default Container;
